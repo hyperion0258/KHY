1390dadc0712e247d55c79edca98bc9a4b60ffbb
KHY <hyperion0258@gmail.com>
2022-02-16 23:28:09 +0900

    2022-02-16 51강

--- photogram/src/main/java/com/cos/photogramstart/service/UserService.java
+++ photogram/src/main/java/com/cos/photogramstart/service/UserService.java
@@ -1,14 +1,14 @@
 package com.cos.photogramstart.service;
 
-import java.util.function.Supplier;
-
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.cos.photogramstart.domain.user.User;
 import com.cos.photogramstart.domain.user.UserRepository;
+import com.cos.photogramstart.handler.ex.CustomException;
 import com.cos.photogramstart.handler.ex.CustomValidationApiException;
+import com.cos.photogramstart.web.dto.user.UserProfileDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,6 +18,23 @@ public class UserService {
 	
 	private final UserRepository userRepository;
 	private final BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	@Transactional(readOnly = true)
+	public UserProfileDto 회원프로필(int pageUserId, int principalId) {
+		UserProfileDto dto = new UserProfileDto();
+		
+		//SELECT * FROM image WHERE userId = :userId;
+		User userEntity = userRepository.findById(pageUserId).orElseThrow(()->{
+			throw new CustomException("해당 프로필 페이지는 없는 페이지입니다.");
+		});
+
+		dto.setUser(userEntity);
+		dto.setPageOwnerState(pageUserId == principalId);
+		dto.setImageCount(userEntity.getImages().size());
+		
+		return dto;
+	}
+	
 	
 	@Transactional
 	public User 회원수정(int id, User user) {
