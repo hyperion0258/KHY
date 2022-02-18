4090eb38af99c27926b7f41ff67583cf1b2e0891
KHY <hyperion0258@gmail.com>
2022-02-18 20:28:23 +0900

    2022-02-18 68강

--- photogram/src/main/java/com/cos/photogramstart/domain/image/Image.java
+++ photogram/src/main/java/com/cos/photogramstart/domain/image/Image.java
@@ -1,6 +1,7 @@
 package com.cos.photogramstart.domain.image;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -9,8 +10,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
+import javax.persistence.Transient;
 
+import com.cos.photogramstart.domain.likes.Likes;
 import com.cos.photogramstart.domain.subscribe.Subscribe;
 import com.cos.photogramstart.domain.user.User;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -32,16 +36,25 @@ public class Image { //N, 1
 	private String caption; // 오늘 나 너무 피곤해!!
 	private String postImageUrl; // 사진을 전송받아서 그 사진을 서버에 특정 폴더에 저장 - DB에 그 저장된 경로를 insert
 	
-	@JsonIgnoreProperties({"image"})
+	@JsonIgnoreProperties({"images"})
 	@JoinColumn(name = "userId")
 	@ManyToOne(fetch = FetchType.EAGER) // 이미지를 select하면 조인해서 User정보를 같이 들고옴
 	private User user; // 1, 1
 	
 	// 이미지 좋아요
+	@JsonIgnoreProperties({"image"})
+	@OneToMany(mappedBy = "image")
+	private List<Likes> likes;
 	
 	// 댓글
 	
 	private LocalDateTime createDate;
+	
+	@Transient // DB에 컬럼이 만들어지지 않는다.
+	private boolean likeState;
+	
+	@Transient
+	private int likeCount;
 	
 	@PrePersist
 	public void createDate() {
